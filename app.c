4cfa04c96ecf96aaf803b2b251f61c07cfa17f7f
oktayturk <oktayturk@gmail.com>
2018-11-07 16:39:37 +0300

    Divide function implemented

--- app.c
+++ app.c
@@ -2,6 +2,15 @@
 #include <stdlib.h>
 #include "inc_a.h"
 
+
+int divide(int x, int y)
+{
+	if (y != 0)
+		return x/y;
+
+	return 0;
+}
+
 int main (int arg, char *argv[])
 {
 	printf( "\nHello World Testbranch1 3!\n" );
